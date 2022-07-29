c433a67b7c5dd2aaf9641cd77b9f1bccb1c07a88
Hamilton Lopes <hamilton_dino@hotmail.com>
2022-07-29 16:39:42 -0300

    getting product info

--- react/Product.tsx
+++ react/Product.tsx
@@ -2,23 +2,20 @@
 import React from 'react'
 import Styles from './Product.module.css'
 
-function Product() {
-  let productImage = `https://blog.calcadonet.com.br/wp-content/uploads/2021/02/estilos-de-roupa-retro.jpg`
-  let productName = `Calça boca de sino`
-  let productPrice = `300`
-  let productInfo = `10x of 30`
+interface propObject {
+  name: string
+  id: number
+  image: string
+  price: any
+}
 
+function Product({ name, image, price, id }: propObject) {
   return (
     <div className={Styles.Wrapper}>
-      <img
-        src={productImage}
-        alt="girl in a cool jeans"
-        width="200"
-        height="300"
-      />
-      <div>{productName}</div>
-      <div>{productInfo}</div>
-      <div>{`R$ ${productPrice},00`}</div>
+      <img src={image} alt="girl in a cool jeans" width="200" height="300" />
+      <div>{name}</div>
+      <div>{`ProductId: ${id}`}</div>
+      <div>{`R$ ${price},00`}</div>
     </div>
   )
 }
--- react/Suggestions.tsx
+++ react/Suggestions.tsx
@@ -2,15 +2,33 @@
 import React from 'react'
 import Product from './Product'
 import Styles from './Suggestions.module.css'
+import { useProduct } from 'vtex.product-context'
 
 function Suggestions() {
+  const productContext: any = useProduct()
+
+  const product = {
+    id: productContext.product.productId,
+    name: productContext.product.productName,
+    price: productContext.product.priceRange.sellingPrice.lowPrice,
+    image: productContext.product.items[0].images[0].imageUrl,
+  }
+
+  const teste = {
+    image: `https://blog.calcadonet.com.br/wp-content/uploads/2021/02/estilos-de-roupa-retro.jpg`,
+    name: `Calça boca de sino`,
+    price: `300`,
+    id: 1030,
+  }
+  console.log(productContext)
+
   return (
     <div className={Styles.Wrapper}>
       <div className={Styles.Title}>Sugestao de Compra</div>
       <div className={Styles.Container}>
-        <Product />
+        <Product {...product} />
         +
-        <Product />= something
+        <Product {...teste} />= something
       </div>
     </div>
   )
