7f4b57d4c89deb1d91f169b2a9adcbcd0c72f959
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-07-30 17:46:30 -0300

    getiing a better component's logic

--- react/Product.tsx
+++ react/Product.tsx
@@ -4,18 +4,28 @@ import Styles from './Product.module.css'
 
 interface propObject {
   name: string
-  id: number
+  info: string
   image: string
   price: any
+  skus: any[]
 }
 
-function Product({ name, image, price, id }: propObject) {
+function Product({ name, image, price, info, skus }: propObject) {
+  console.log(skus)
   return (
     <div className={Styles.Wrapper}>
       <img src={image} alt="girl in a cool jeans" width="200" height="300" />
       <div>{name}</div>
-      <div>{`ProductId: ${id}`}</div>
       <div>{`R$ ${price},00`}</div>
+      <div>{`${info}`}</div>
+      {skus.map((sku, index) => {
+        console.log('passei aqui')
+        return (
+          <button type="button" key={index}>
+            {sku.name}
+          </button>
+        )
+      })}
     </div>
   )
 }
--- react/Suggestions.tsx
+++ react/Suggestions.tsx
@@ -3,15 +3,26 @@ import React from 'react'
 import Product from './Product'
 import Styles from './Suggestions.module.css'
 import { useProduct } from 'vtex.product-context'
+import { useQuery } from 'react-apollo'
+import productsByIdentifier from './queries/productByIdentifier.graphql'
 
 function Suggestions() {
   const productContext: any = useProduct()
+  const melhoresCombinacoes = [54, 23, 6]
+
+  const { data } = useQuery(productsByIdentifier, {
+    variables: { field: 'id', values: melhoresCombinacoes },
+  })
+
+  console.log(data)
 
   const product = {
     id: productContext.product.productId,
     name: productContext.product.productName,
-    price: productContext.product.priceRange.sellingPrice.lowPrice,
+    price: productContext.product.items[0].sellers[0].commertialOffer.Price,
     image: productContext.product.items[0].images[0].imageUrl,
+    info: `Em até 3x R$${productContext.product.items[0].sellers[0].commertialOffer.Installments[2].Value}, ${productContext.product.items[0].sellers[0].commertialOffer.Installments[2].InterestRate}% de juros.`,
+    skus: productContext.product.items,
   }
 
   const teste = {
@@ -19,6 +30,8 @@ function Suggestions() {
     name: `Calça boca de sino`,
     price: `300`,
     id: 1030,
+    info: `Em até 3x R$100,00, 0% de juros.`,
+    skus: [0],
   }
   console.log(productContext)
 
